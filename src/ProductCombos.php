8655078128581d4083f13b7e0a56e45b8e8d75c1
23d1 <a@23d.one>
2019-09-03 10:53:49 -0700

    Initial commit

---
+++ src/ProductCombos.php
@@ -0,0 +1,73 @@
+<?php
+namespace modules\productcombos;
+
+use Craft;
+
+use yii\base\Event;
+use yii\base\Module;
+
+use craft\events\RegisterComponentTypesEvent;
+use craft\commerce\services\OrderAdjustments;
+
+// use modules\productcombos\adjusters\LineItemAdjuster;
+use modules\productcombos\adjusters\OrderAdjuster;
+
+class ProductCombos extends Module
+{
+    // Static Properties
+    // =========================================================================
+
+    /**
+     * Static property that is an instance of this module class so that it can be accessed via
+     * ProductCombos::$instance
+     *
+     * @var ProductCombos
+     */
+    public static $instance;
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct($id, $parent = null, array $config = [])
+    {
+        // Set this as the global instance of this module class
+        static::setInstance($this);
+
+        parent::__construct($id, $parent, $config);
+    }
+
+    /**
+     * Called after the module class is instantiated; do any one-time initialization
+     * here such as hooks and events.
+     *
+     * Set our $instance static property to this class so that it can be accessed via
+     * Utils::$instance
+     *
+     */
+    public function init()
+    {
+        parent::init();
+        self::$instance = $this;
+
+        // The order you add these is important! Adjusters run on the current total(s)
+        // of the order, so if you are working with percentage-based discounts, be
+        // careful how you organize these. Below, we're “unshift-ing” them (in argument
+        // order) onto the `$event->types` array. LineItem adjustments will happen
+        // before the complete Order adjustment is calculated.
+        Event::on(
+            OrderAdjustments::class,
+            OrderAdjustments::EVENT_REGISTER_ORDER_ADJUSTERS,
+            function(RegisterComponentTypesEvent $event) {
+                array_unshift(
+                    $event->types,
+                    // LineItemAdjuster::class,
+                    OrderAdjuster::class
+                );
+
+                return $event->types;
+            });
+    }
+}
---
+++ src/adjusters/OrderAdjuster.php
@@ -0,0 +1,97 @@
+<?php
+namespace modules\productcombos\adjusters;
+
+use craft\commerce\base\AdjusterInterface;
+use craft\commerce\elements\Order;
+use craft\commerce\models\OrderAdjustment;
+
+use modules\productcombos\ProductCombos as ProductCombos;
+
+class OrderAdjuster implements AdjusterInterface
+{
+    // Constants
+    // =========================================================================
+
+    /**
+     * The discount adjustment type.
+     */
+    const ADJUSTMENT_TYPE = 'Discount';
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public function adjust(Order $order): array
+    {
+        $adjustments = [];
+
+        $items = [];
+        $cartvariants = [];
+        $qty = [];
+
+        foreach ($order->lineItems as $lineItem) {
+            $items[] = $lineItem->purchasable->product;
+            $cartvariants[] = $lineItem->purchasable->id;
+            $qty[] = $lineItem->qty;
+        }
+
+        $categories = \craft\elements\Category::find()
+            ->group('productPacks')
+            ->all();
+
+        foreach ($categories as $category) {
+            // unset($adjustment);
+            $adjustment = $this->_getEmptyOrderAdjustmentFor($order);
+            $discount = 0;
+
+            // unset($relateditems);
+            // unset($relatedvariants);
+            $relateditems = [];
+            $relatedvariants = [];
+
+            foreach ($category->relatedProducts->all() as $product) {
+                array_push($relateditems, $product);
+            }
+            foreach ($category->productVariants->all() as $variant) {
+                array_push($relatedvariants, $variant->id);
+            }
+
+            if (!array_diff($relateditems, $items) && !array_diff($cartvariants, $relatedvariants)) {
+                $multiplier = 0;
+                if ($qty > 1) {
+                    $multiplier = min($qty);
+                }
+
+                $adjustment->description = $category->title;
+                $discount = $category->productPackDiscountAmount;
+
+                // Set discount amount
+                $adjustment->amount = -$discount * $multiplier;
+
+                $adjustments[] = $adjustment;
+                // array_push($adjustments, $adjustment);
+            }
+        }
+
+        return $adjustments;
+    }
+
+    // Private Methods
+    // =========================================================================
+
+    private function _getEmptyOrderAdjustmentFor(Order $order)
+    {
+        $adjustment = new OrderAdjustment();
+        $adjustment->type = self::ADJUSTMENT_TYPE;
+        $adjustment->name = 'Combo';
+        $adjustment->orderId = $order->id;
+        $adjustment->description = 'Product combination discount.';
+        $adjustment->sourceSnapshot = [
+            'PrivateProp' => 'Criteria you want to make sure you have access to, in case you have to recalculate, later!'
+        ];
+
+        return $adjustment;
+    }
+}
